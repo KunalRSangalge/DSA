cd1471c5bfad3ac8c2804bb3a4802f303920fb46
kunal <kunal.sangalge@gmail.com>
2024-06-16 09:25:06 +0530

    Peak in a mountain array

---
+++ Binary search/peak.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cout<<"enter size"<<endl;
+    cin>>n;
+    int a[n];
+    cout<<"enter ele"<<endl;
+    for(int i=0;i<n;i++)
+    cin>>a[i];
+
+    int s=0,e=n-1,mid;
+    
+    while(s<e)
+    {
+        mid=(s+e)/2;
+
+        if(a[mid]<a[mid+1])
+        s=mid+1;
+        else
+        e=mid;
+    }
+    cout<<s;
+}
