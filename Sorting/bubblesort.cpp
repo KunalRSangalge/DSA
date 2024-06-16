4e6c4d08e29edfe4eeeeba35c30fda6b3ec26e8a
kunal <kunal.sangalge@gmail.com>
2024-06-16 18:56:19 +0530

    Add bool to reduce Time Complexity

--- Sorting/bubblesort.cpp
+++ Sorting/bubblesort.cpp
@@ -11,13 +11,17 @@ int main()
     for(int i=0;i<n;i++)
     cin>>a[i];
 
+    bool flag=true;
+
     for(int i=0;i<n;i++)
     {
         for(int j=0;j<n-1-i;j++)
         {
             if(a[j]>a[j+1])
-            swap(a[j],a[j+1]);
+            {swap(a[j],a[j+1]); flag=false;}
         }
+        if(flag==false)
+        break; 
     }
     for(int i=0;i<n;i++)
     cout<<a[i]<<' ';
