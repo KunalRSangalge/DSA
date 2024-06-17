13deca44b605f2adfb9be246ac9cf3aa14eba67d
kunal <kunal.sangalge@gmail.com>
2024-06-17 12:28:17 +0530

    Min of rotated sorted array

---
+++ Binary search/min.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cout<<"enter size"<<endl;
+    cin>>n;
+    int a[n];
+    cout<<"enter ele"<<endl;
+    for(int i=0;i<n;i++)
+    cin>>a[i];
+
+    int s=0,e=n-1,mid;
+    while(s<e)
+    {
+        mid=(s+e)/2;
+
+        if(a[0]<a[n-1])
+        {cout<<a[0];exit(0);}
+        else
+        {
+            if(a[mid]>a[0])
+            s=mid+1;
+            else
+            e=mid;
+        }
+        
+    }
+    cout<<a[s];
+}
