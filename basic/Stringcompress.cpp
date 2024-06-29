2e53da1724ed82973801711eccc9106df3333b04
kunal <kunal.sangalge@gmail.com>
2024-06-29 21:36:12 +0530

    optimised solution

--- basic/Stringcompress.cpp
+++ basic/Stringcompress.cpp
@@ -1,62 +1,29 @@
 #include<iostream>
 #include<vector>
 using namespace std;
-int digits(int n)
-    {
-        int digit=0,i=0,temp=n;
-        while(temp!=0)
-        {
-            temp/=10;
-            digit++;
-        }
-        return digit;
-         
-    }
-int compress(vector<char>& chars) {
-           char ch;
-        int i=0,count[26]={0};
-        while(i<chars.size())
-        {
-            ch=chars[i];
-            int x = ch -'a';
-            count[x]++;
-            i++;            
-        }
-        
-        int j=0;
-        for(int i=0;i<26;i++)
-        {
-            if(count[i]>0)
-            {
-                ch = i + 'a';
-                chars[j]=ch;
 
-                if(count[i]!=1 && count[i]<10)
-                {chars[j+1]=count[i]+48;j+=2;}
-                else if(count[i]>=10)
-                {
-                    int x=digits(count[i]);
-                    int t=x;
-                    int y=count[i];
-                    while(x!=0)
-                    {                        
-                        chars[j+x]=(y%10) + 48;
-                        y/=10;
-                        x--;
-                    }
-                    j+=t+1;
-                }
-                else
-                j++;
-            }
+int compress(vector<char>& chars) {
+            int i = 0, j = 0, n = chars.size(), ansIndex = 0;
+        char ch;
+        while (i < n && j < n)
             
-        }
-        int t=j;
-        for(int i=j;i<chars.size();i++)
         {
-            chars[i]='\0';
+            j = i + 1;
+            while (j < n && chars[i] == chars[j])
+                j++;
+
+            chars[ansIndex++] = chars[i];
+            int count = j - i;
+
+            if (count > 1) {
+                string str = to_string(count);
+                for (char ch : str)
+                    chars[ansIndex++] = ch;
+            }
+
+            i = j;
         }
-        return j;
+        return ansIndex;
     }
 int main()
 {
