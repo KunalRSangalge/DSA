aa1ca73c71a424c43f30bb350617d009f058aa03
kunal <kunal.sangalge@gmail.com>
2024-07-13 13:17:02 +0530

    Optimised solution

--- Binary search/Searchinrotsortarr.cpp
+++ Binary search/Searchinrotsortarr.cpp
@@ -1,46 +1,29 @@
-//finding pivot and then getting answer 
-//failed test case: nums=[3,1]
+//checking which part is sorted and then eliminating that part if target not present
+//O(logn)
 #include<bits/stdc++.h>
 using namespace std;
 
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
-        int s = 0, e = nums.size() - 1, mid, p;
-        while (s < e) {
-            mid = (s + e) / 2;
+        int n = nums.size()-1;
+        int s=0,e=n,mid;
+        while(s<=e){
+            mid=(s+e)/2;
+            if(nums[mid]==target)
+            return mid;
 
-            if (nums[mid] > nums[0])
-                s = mid + 1;
-            else
-                e = mid;
-        }
-        p = s;
-        s = 0, e = nums.size() - 1;
-
-        if (target >= nums[p] && target <= (nums[nums.size()-1])) {
-            s = p;
-            while (s <= e) {
-                mid = (s + e) / 2;
-
-                if (target == nums[mid])
-                    return mid;
-                else if (target > nums[mid])
-                    s = mid + 1;
+            if(nums[mid] >= nums[s]){
+                if(target <= nums[mid] && target >= nums[s])
+                e=mid-1;
                 else
-                    e = mid - 1;
+                s=mid+1;
             }
-        } else {
-            e = p;
-            while (s <= e) {
-                mid = (s + e) / 2;
-
-                if (target == nums[mid])
-                    return mid;
-                else if (target > nums[mid])
-                    s = mid + 1;
+            else{
+                if(target >= nums[mid] && target <= nums[e])
+                s=mid+1;
                 else
-                    e = mid - 1;
+                e=mid-1;
             }
         }
         return -1;
