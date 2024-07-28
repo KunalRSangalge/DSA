f6e0be7d1edfdbf680ecba9334a346de7098bfa3
Kunal Rajesh Sangalge <kunal.sangalge@gmail.com>
2024-07-28 14:00:13 +0530

    Vector iterator

--- Pointers/test.cpp
+++ Pointers/test.cpp
@@ -1,5 +1,18 @@
-#include<iostream>
+#include<bits/stdc++.h>
 using namespace std;
 int main(){
-    cout<<"hello world";
+    vector<int>p;
+    p.push_back(1);
+    p.push_back(2);
+    p.push_back(3);
+    p.push_back(14);
+    p.push_back(5);
+
+    vector<int>::iterator it=p.begin();
+    it++;
+    cout<<*(it)<<" ";
+
+    for(vector<int>::iterator itt=p.begin();it!=p.end();it++){
+        cout<<*(it)<<endl;
+    }
 }
