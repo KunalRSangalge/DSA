2b48ec1000e0d1bc2497b890317f241ed1dbe160
Kunal Rajesh Sangalge <kunal.sangalge@gmail.com>
2024-08-06 16:20:21 +0530

    next permutation lc31

---
+++ basic/array/nextpermutation.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+void nextPermutation(vector<int>& nums) {
+        int n=nums.size()-1;
+        int i=n;
+        while(i>0){
+            if(nums[i]>nums[i-1]){
+                swap(nums[i],nums[i-1]);
+                if(i==1){
+                    sort(nums.begin()+1,nums.end());
+                }
+                break;
+            }
+            
+        }
+        
+    }
+int main(){
+    vector<int> arr={1,1,5};
+    nextPermutation(arr);
+    for(int i=0;i<arr.size();i++) cout<<arr[i];
+}
