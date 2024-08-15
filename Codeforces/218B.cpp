8d70a4154829ba15b86162b0b9f3cfcb86336907
Kunal Rajesh Sangalge <kunal.sangalge@gmail.com>
2024-08-15 15:05:21 +0530

    using recursion TLE

--- Codeforces/218B.cpp
+++ Codeforces/218B.cpp
@@ -1,25 +1,49 @@
 #include<bits/stdc++.h>
 using namespace std;
-void solve(int a[],int n,int m, int idx,int sum,vector<int>&ans){
-   
-    if(idx>=m){
+//this is right but TLE
+void solve(int a[],int n,int m,int sum,vector<int>&ans){
+    if(n<0) return ;
+    if(n==0){
         ans[0]=max(ans[0],sum);
-        ans[1]=min(ans[1],sum);
-        return;
+        ans[1]=min(ans[1],sum);return;
     }
-    for(int i=0;i<n;i++){
-        //include
-        if(a[i]!=0){
-            sum+=a[i];
-            a[i]--;
-            solve(a,n,m,idx+1,sum,ans);
-            sum-=a[i];
-        }
-        //exclude
-        solve(a,n,m,idx+1,sum,ans);
-                
+    for(int i=0;i<m;i++){
+        if(a[i]>0){
+        sum+=a[i];
+        a[i]--;
+        solve(a,n-1,m,sum,ans);
+        a[i]++;
+        sum-=a[i];}
     }
+
 }
+//This function does not take all cases
+// void solve(int a[],int n,int m, int idx,int sum,vector<int>&ans){
+//     //base case
+//     if(n<0) return;
+//     if(n==0){
+//         ans[0]=max(ans[0],sum);
+//         ans[1]=min(ans[1],sum);
+//         return;
+//     }
+//     if(idx>=m){
+//         return;
+//     }
+    
+//         //include
+//         if(a[idx]!=0){
+//            // sum+=a[idx];
+//           //  int t=a[idx];
+//             a[idx]--;
+//             solve(a,n-1,m,idx,sum+a[idx],ans);
+//           //  sum-=t;
+//             a[idx]++;
+//         }
+        
+        
+//         //exclude
+//         solve(a,n,m,idx+1,sum,ans);   
+// }
 int main(){
     int n,m;
     cin>>n>>m;
@@ -29,7 +53,8 @@ int main(){
     vector<int>ans;
     ans.push_back(0);
     ans.push_back(INT_MAX);
-    solve(a,n,m,0,sum,ans);
+    //solve(a,n,m,0,sum,ans);
+    solve(a,n,m,0,ans);
     cout<<ans[0]<<" "<<ans[1];
     
 }
