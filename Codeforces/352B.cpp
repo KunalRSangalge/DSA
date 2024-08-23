2a306e860f5acb3ae97948707a539f68613a34db
Kunal Rajesh Sangalge <kunal.sangalge@gmail.com>
2024-08-23 23:55:29 +0530

    optimised

--- Codeforces/352B.cpp
+++ Codeforces/352B.cpp
@@ -1,46 +1,36 @@
 #include<bits/stdc++.h>
 using namespace std;
-bool notp(int a[],int j){
-    int k=a[j];
-    j--;
-    while(j>=0){
-        if(k==a[j]) return false;
-        j--;
+bool ap(vector<int>v){
+    if(v.size()==1)return true;
+    int d=v[1]-v[0];
+    for(int i=1;i<v.size();i++){
+        if((v[i]-v[i-1])!=d) return false;
     }
     return true;
+}
+int ans1(vector<int>v){
+    if(v.size()==1)return 0;
+    return v[1]-v[0];
 }
 int main(){
     int n;
     cin>>n;
     int a[n];
     for(int i=0;i<n;i++)cin>>a[i];
-
-    int count=0;
-    map<int,int>mpp;
-for(int i=0;i<n;i++){
-    int idx=i,d=0;
-    bool flag=true;
-    if(notp(a,i)){
-        for(int j=i+1;j<n;j++){        
-        if(a[i]==a[j]){
-        if(d==0){
-            d=j-i;
-            idx=j;
-        }
-        else{
-            if(d!=(j-idx)) flag=false;
-            else{
-                    idx=j;
-                }
-            }
-        }
-    
+    map<int,vector<int>>mpp;
+    for(int i=0;i<n;i++){
+        mpp[a[i]].push_back(i);
     }
-        if(flag){count++;mpp[a[i]]=d;}
-    }    
-}
-cout<<count<<endl;
-for(auto x : mpp){
-    cout<<x.first<<" "<<x.second<<endl;
-}
+    int count=0;
+    map<int,int>ans;
+    for(auto x : mpp){
+        if(ap(x.second)){
+            count++;
+            ans[x.first]=ans1(x.second);
+        }
+    }
+    cout<<count<<endl;
+    for(auto x : ans){
+        cout<<x.first<<" "<<x.second<<endl;
+    }
 }
