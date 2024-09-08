6bee8f569c94595e220ec112231c0d9c4e1b6bc9
Kunal Rajesh Sangalge <kunal.sangalge@gmail.com>
2024-09-08 22:48:18 +0530

    800

--- Codeforces/1761A.cpp
+++ Codeforces/1761A.cpp
@@ -1,15 +1,5 @@
 #include<bits/stdc++.h>
 using namespace std;
-// bool check(vector<int>arr,vector<int>v,int a, int b){
-//     bool flag = true;
-//     for(int i=0;i<a;i++){
-//         if(arr[i]!=v[i])flag=false;
-//     }
-//     for(int i=arr.size()-1;i>(arr.size()-b-1);i--){
-//         if(arr[i]!=v[i]) flag=false;
-//     }
-//     return flag;
-// }
 int main(){
     int t;
     cin>>t;
