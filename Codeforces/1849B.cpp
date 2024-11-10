0068ba651173b7372853354320d8da194cc48e8f
Kunal Rajesh Sangalge <kunal.sangalge@gmail.com>
2024-11-10 19:22:18 +0530

    good sorting function

--- Codeforces/1849B.cpp
+++ Codeforces/1849B.cpp
@@ -1,45 +1,29 @@
 #include<bits/stdc++.h>
 #define ll long long int
 using namespace std;
-bool check(vector<pair<ll,ll>>v){
-    for(ll i=0;i<v.size();i++){
-        if(v[i].second > 0) return true;
-    }
-    return false;
-}
-bool cmp(pair<ll,ll>&left , pair<ll,ll>&right){
-    if(left.second == right.second){
-        if(left.first < right.first) return true;
-        else return false;
-    }
-    return left.second > right.second;
+bool cmp(pair<ll,ll> &a,pair<ll,ll>&b){
+    if(a.first==b.first) return a.second<b.second;
+    return a.first>b.first;
 }
 int main(){
-    int t;
+    ll t;
     cin>>t;
     while(t--){
         ll n,k;
         cin>>n>>k;
-        ll a[n];
-        for(ll i=0;i<n;i++){
-            cin>>a[i];
-        }
-
         vector<pair<ll,ll>>v;
-        for(ll i=0; i<n; i++){
-            v.push_back(make_pair(i+1,a[i]));
+        
+        for(ll i=0;i<n;i++){
+            ll in;
+            cin>>in;
+
+            if(in%k) in%=k;
+            else in=k;
+            v.push_back(make_pair(in,i+1));
         }
 
-        vector<ll>ans;
-        while(check(v)){
-            sort(v.begin(),v.end(),cmp);
-            v[0].second -= k;
-            if(v[0].second <= 0) ans.push_back(v[0].first);
-        }
-        for(auto x : ans){
-            cout<<x<<" ";
-        }
-        cout<<endl;
-
+       sort(v.begin(),v.end(),cmp);
+       for(int i=0;i<n;i++) cout<<v[i].second<<" ";
+       cout<<endl;
     }
 }
