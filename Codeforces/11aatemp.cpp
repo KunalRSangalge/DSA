0c6ee54901ef961703b457297074470c6bb83f7f
Kunal Rajesh Sangalge <kunal.sangalge@gmail.com>
2024-11-12 18:08:24 +0530

    updated

--- Codeforces/11aatemp.cpp
+++ Codeforces/11aatemp.cpp
@@ -1,6 +1,22 @@
 #include<bits/stdc++.h>
 #define ll long long int
 using namespace std;
+vector<ll> div(ll n){
+    vector<ll>div;
+    for(ll i=1;i<=sqrt(n);i++){
+        if(n%i==0){
+            div.push_back(i);
+            div.push_back(n/i);
+        }
+    }
+    return div;
+}
+void yes(){
+    cout<<"YES"<<endl;
+}
+void no(){
+    cout<<"NO"<<endl;
+}
 int main(){
     int t;
     cin>>t;
