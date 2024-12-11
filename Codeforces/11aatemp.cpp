cf19223cfc39193c1eae45d9a5ea4baf7438def9
Kunal Rajesh Sangalge <kunal.sangalge@gmail.com>
2024-12-11 11:20:23 +0530

    added sieve

--- Codeforces/11aatemp.cpp
+++ Codeforces/11aatemp.cpp
@@ -1,6 +1,20 @@
 #include<bits/stdc++.h>
 #define ll long long int
+#define N 1e12
 using namespace std;
+vector<bool>prime(N+1,true);
+// O(NloglogN) sieve of erastosthanes
+void p(){
+    prime[0]=false;
+    prime[1]=false;
+    for(ll i=0;i<N;i++){
+        if(prime[i]){
+            for(ll j=2*i;j<N;j+=i){
+                prime[j]=false;
+            }
+        }
+    }
+}
 vector<ll> div(ll n){
     vector<ll>div;
     for(ll i=1;i<=sqrt(n);i++){
